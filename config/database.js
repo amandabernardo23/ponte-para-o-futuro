539e0a8fd8176e1c8ec7e52246cba0cc4d51c495
ClovisVitor <clovisvitor.04@gmail.com>
2025-05-11 15:14:20 -0300

    Adicionando mudanças as variaveis de ambientes

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require("dotenv").config();
 
 // Conexão com o banco de dados 
 const db = mysql.createConnection({
-   host: 'caboose.proxy.rlwy.net',
-  user: 'root',
-  password: 'QTRiYxwKeMYmiWzlXXnNUnxFUMKYROWt',//senha do banco hospedado no Railway
-  database: 'railway',
-  port: 50541
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,//senha do banco hospedado no Railway
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
