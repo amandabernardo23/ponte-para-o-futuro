d8824862e8513e66da8e4470a5dbf6e7d01ad94c
ClovisVitor <clovisvitor.04@gmail.com>
2025-05-19 15:25:11 -0300

    Alterações de Variáveis para cadastro de Projetos

--- controllers/projetosController.js
+++ controllers/projetosController.js
@@ -15,16 +15,16 @@ exports.listarProjetos = (req, res) => {
 
 // Controlador para cadastrar um novo projeto
 exports.cadastrarProjeto = (req, res) => {
-  const { titulo, descricao, status, data_inicio, data_termino, id_empresa } = req.body;
+  const { titulo, descricao, status, data_inicio, data_termino, id_universidade } = req.body;
 
   // Validação simples
-  if (!titulo || !status || !data_inicio || !data_termino || !id_empresa) {
+  if (!titulo || !status || !data_inicio || !data_termino || !id_universidade) {
     return res.status(400).json({ mensagem: 'Preencha todos os campos obrigatórios!' });
   }
 
-  const sql = 'INSERT INTO projetos (título, descrição, status, data_inicio, data_termino, id_empresa) VALUES (?, ?, ?, ?, ?, ?)';
+  const sql = 'INSERT INTO projetos (título, descrição, status, data_inicio, data_termino, id_universidade) VALUES (?, ?, ?, ?, ?, ?)';
 
-  db.query(sql, [titulo, descricao, status, data_inicio, data_termino, id_empresa], (err, result) => {
+  db.query(sql, [titulo, descricao, status, data_inicio, data_termino, id_universidade], (err, result) => {
     if (err) {
       console.error('Erro ao cadastrar projeto:', err);
       return res.status(500).json({ mensagem: 'Erro ao cadastrar projeto' });
