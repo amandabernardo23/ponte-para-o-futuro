c78ddd9b4a863d5a9aa3297ce7b6316493973cc5
ClovisVitor <clovisvitor.04@gmail.com>
2025-06-16 17:47:38 -0300

    ajustando controller do perfil

--- routes/perfil.js
+++ routes/perfil.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const perfilControllers = require('../controllers/perfilControllers');
 
 router.get('/perfil/:usuarioId', perfilControllers.buscarPerfil);
-router.post('/perfil/:usuarioId', perfilControllers.salvarPerfil);
+router.post('/perfil/:usuarioId', (req, res, next) => {
+  console.log(`POST /api/perfil/${req.params.usuarioId} recebido`);
+  perfilControllers.salvarPerfil(req, res);
+});
 
 module.exports = router;
